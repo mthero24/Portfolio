a80018c03ccf292cc83da3c5e00b4dc8887e6ec2
Michael Thero <mthero24@gmail.com>
2019-03-16 15:31:50 -0400

    initiated new and create actions for my portfolios

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -2,4 +2,22 @@ class MyPortfoliosController < ApplicationController
     def index
         @portfolio = MyPortfolio.all
     end
+
+    def new
+        @portfolio = MyPortfolio.new    
+    end
+
+    def create
+        @portfolio = MyPortfolio.new(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio.save
+            format.html { redirect_to @portfolio, notice: 'Portfoilio was successfully created.' }
+            format.json { render :my_portfolios, status: :created, location: @portfolio }
+          else
+            format.html { render :new }
+            
+          end
+        end
+      end
 end
